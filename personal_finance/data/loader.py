547f997fc1a86102d17ae0f71aa5205f13620ce5
llazzari <lucas.lazzari@outlook.com>
2024-03-05 13:53:35 -0300

    refactor: remove unused function

--- personal_finance/data/loader.py
+++ personal_finance/data/loader.py
@@ -4,15 +4,9 @@ from pathlib import Path
 from data.schema import DataSchema
 from data.processor import sort_by_date
 
-DATA_PATH = Path.cwd() / "data" / "expenses.csv"
 
-
-def expenses_file_exists() -> bool:
-    return DATA_PATH.exists()
-
-
-def load_data() -> pd.DataFrame:
-    if not expenses_file_exists():
+def load_data(file_path: Path) -> pd.DataFrame:
+    if not file_path.exists():
         return pd.DataFrame()
     dtype: dict[str, type] = {
         DataSchema.YEAR: int,
@@ -25,18 +19,9 @@ def load_data() -> pd.DataFrame:
         DataSchema.DESCRIPTION: str,
     }
     df = pd.read_csv(
-        DATA_PATH,
+        file_path,
         dtype=dtype,
         usecols=list(dtype.keys()),
     )
     df['id'] = df.index
     return sort_by_date(df)
-
-
-def set_table_data(new_df: pd.DataFrame) -> pd.DataFrame:
-    old_df: pd.DataFrame = load_data()
-
-    df = pd.concat([new_df, old_df])
-    if not df.empty:
-        df.to_csv(DATA_PATH, index=False, float_format='%.2f')
-    return df
