7f577990d693679311c9e9f249e37e9fc16e7f02
llazzari <lucas.lazzari@outlook.com>
2024-03-07 11:06:02 -0300

    refactor: add protocol classes for model and vectorizer

--- personal_finance/data/ml/predictor.py
+++ personal_finance/data/ml/predictor.py
@@ -1,12 +1,25 @@
 from pathlib import Path
+from typing import Protocol
 import joblib
+import numpy as np
 import pandas as pd
+from scipy.sparse import spmatrix
 
 from data.raw.cleaner import clean_descriptions
 from data.schema import DataSchema
 
 MODEL_FILE: Path = Path.cwd() / 'data' / 'ml' / 'model.joblib'
 VECTORIZER_FILE: Path = Path.cwd() / 'data' / 'ml' / 'vectorizer.joblib'
+
+
+class MLModel(Protocol):
+    def predict(self, X: np.ndarray | spmatrix) -> np.ndarray:
+        ...
+
+
+class Vectorizer(Protocol):
+    def transform(self, X: np.ndarray) -> spmatrix:
+        ...
 
 
 def separate_data(df: pd.DataFrame) -> list[pd.DataFrame]:
@@ -19,9 +32,9 @@ def predict_subcategories(uncategorized_df: pd.DataFrame) -> pd.DataFrame:
 
     df = clean_descriptions([], uncategorized_df)
 
-    model = joblib.load(MODEL_FILE)
-    vectorizer = joblib.load(VECTORIZER_FILE)
-    X_vectorized = vectorizer.transform(
+    model: MLModel = joblib.load(MODEL_FILE)
+    vectorizer: Vectorizer = joblib.load(VECTORIZER_FILE)
+    X_vectorized: spmatrix = vectorizer.transform(
         df[DataSchema.CLEANED_DESCRIPTION].to_numpy(dtype='<U21')
     )
     df.loc[:, DataSchema.SUBCATEGORY] = model.predict(X_vectorized)
